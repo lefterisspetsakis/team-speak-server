c48c8c8daee4731f00d20567569a32c6889eea44
Lefteris Spetsakis <left.plan@gmail.com>
2017-03-16 23:01:03 -0400

    docker_bootstrap.sh can now install teamspeak

--- docker_bootstrap.sh
+++ docker_bootstrap.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 
 apt-get update
+adduser --disabled-login teamspeak
+
+mkdir /home/tsdownloads/
+
+wget http://dl.4players.de/ts/releases/3.0.12.4/teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2 -P /home/tsdownloads/
+tar xvf /home/teamspeak3-server_linux_amd64-3.0.12.4.tar.bz2 -C /home/teamspeak/ --strip-components=1
+chown -R teamspeak:teamspeak /home/teamspeak
+
+
+
+
+
+
+
+
 #DEBIAN_FRONTEND="noninteractive" apt-get -qy install \
 #  git-all \
 #  git-hub \
